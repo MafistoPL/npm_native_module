a7273412c0063bb54d5df963d4747c5dd58ea063
Mateusz Poreba <mateusz.poreba@outlook.com>
2022-11-07 11:49:56 +0100

    fix issue

--- hello.cpp
+++ hello.cpp
@@ -13,12 +13,12 @@ namespace demo
   {
     Isolate* isolate = args.GetIsolate();
     args.GetReturnValue().Set(String::NewFromUtf8(
-        isolate, "world").ToLocalChecked());
+        isolate, "Hello from cpp code!").ToLocalChecked());
   }
 
   void Initialize(Local<Object> exports) 
   {
-    NODE_SET_METHOD(exports, "Hello from cpp code!", Method);
+    NODE_SET_METHOD(exports, "hello", Method);
   }
 
   NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const addon = require('./build/Release/addon');
 
+console.log(addon.hello());
+
 module.exports = addon;
